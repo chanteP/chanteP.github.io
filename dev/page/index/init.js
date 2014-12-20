28ac1939a41d7462952b247dfce81162956ab2b8
chanteP <541149506@qq.com>
2014-12-21 02:59:49 +0800

    bt

--- dev/page/index/init.js
+++
@@ -1,8 +0,0 @@
-window.parent.core.initPage(document, function(wrap, $){
-    console.log('index')
-    return {
-        init : function(){
-    console.log('index init')
-        }
-    }
-});
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var args = process.argv.slice(2);
-
-if(args.indexOf('publish') >= 0){
-    require('./dev/bin/package')();
-    require('./dev/bin/build')();
-}
-else if(args.indexOf('clean') >= 0){
-    require('child_process').exec('rm -r static');
-    require('child_process').exec('rm -r page');
-    require('./dev/bin/tabs').forEach(function(tab){
-        require('child_process').exec('rm ' + tab + '.html');
-    });
-}
-else{
-    require('./dev/bin/package')(true);
-    require('./dev/bin/server');
+if(!(__dirname.slice(-3) == 'dev')){
+    if(args.indexOf('publish') >= 0){
+        // require('./dev/bin/package')();
+        require('./dev/bin/build')();
+    }
+    else if(args.indexOf('clean') >= 0){
+        clean();
+    }
+    else{
+        require('./dev/bin/package')(true);
+        require('./dev/bin/server');
+    }
+    function clean(){
+        require('child_process').exec('rm -r static');
+        require('child_process').exec('rm -r pages');
+        require('./dev/bin/tabs').forEach(function(tab){
+            require('child_process').exec('rm ' + tab + '.html');
+        });
+    }
 }
