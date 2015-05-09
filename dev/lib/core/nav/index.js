be335c8ef5a38d72680994325d0aa6e1cc53b84a
chanteP <541149506@qq.com>
2015-05-09 21:23:28 +0800

    test

--- dev/lib/core/nav/index.js
+++ dev/lib/core/nav/index.js
@@ -6,7 +6,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
     nav.set = function(page){
         var cur = $.find('.cur', nav);
-        var to = $.find('[data-for="'+page+'"]', nav);
+        var to;
+        [].forEach.some.call($.find('[data-for]', nav), function(li){
+            if(li.dataset['for'].split(',').indexOf(page) >= 0){
+                to = li;
+                return true;
+            }
+        });
         cur && cur.classList.remove('cur');
         to && to.classList.add('cur');
         to && (to.dataset.hide ? this.hide() : this.show());
