5192bdea3fea8a19de423ef8ce1248e379a7c72a
chanteP <541149506@qq.com>
2015-05-10 19:08:22 +0800

    deploy fix

--- bin/deployMaster.js
+++ bin/deployMaster.js
@@ -6,97 +6,125 @@ var gulp = require('gulp'),
     replace = require('gulp-replace'),
     build = require('./gulp-buildPage');
 
+var log = function(msg){
+    console.log(msg)
+}
 module.exports = function(env){
+    var min = env !== 'development'
+    var pageEngine = function*(msg){
+
+        var pageBuildModNum = 4, i = 0;
+        while(++i <= pageBuildModNum){
+            log(yield, i);
+        }
+        gulp.start('pages')
+    }
+    pageEngine = pageEngine();
+
     gulp.task('layout', function(){
         //装饰器
+        log('[root]复制装饰器');
         return gulp.src(['dev/dec/*.html'])
             .pipe(replace('<%-page%>'), '{{ content }}')
             .pipe(gulp.dest('built/_layout/'));
     });
     gulp.task('post', function(){
         //菠萝格
+        log('[root]复制post');
         return gulp.src(['post/*'])
             .pipe(gulp.dest('built/_post/'));
     });
     gulp.task('rootConfig', function(){
         //根目录配置文件
+        log('[root]复制配置文件');
         return gulp.src(['root/*'])
             .pipe(gulp.dest('built/'));
     });
 
     gulp.task('pages', function(){
         //一个页面对应一个文件夹
+        log('[html]页面生成');
         gulp.src(['dev/page/*/index.html'])
             .pipe(rename(function(file){
                 file.basename = file.dirname;
                 file.dirname = '';
             }))
-            .pipe(build())
+            .pipe(build({min:min}))
             .pipe(header([
                 '---',
                 'layout: main',
-                '---'
+                '---',
+                ''
             ].join('\n')))
-            .pipe(gulp.dest('built/'));
+            .pipe(gulp.dest('built/'))
+            .on('finish', function(){log('[html]页面生成完成')});
+        log('[html]单页生成');
         gulp.src(['dev/page/*/index.html'])
             .pipe(rename(function(file){
                 file.basename = file.dirname;
                 file.dirname = '';
             }))
-            .pipe(build())
+            .pipe(build({min:min}))
             .pipe(header([
                 '---',
                 'layout: page',
-                '---'
+                '---',
+                ''
             ].join('\n')))
-            .pipe(gulp.dest('built/pages/'));
+            .pipe(gulp.dest('built/pages/'))
+            .on('finish', function(){log('[html]单页生成完成')});
     });
     gulp.task('pageResources', function(){
-        //sass编译
+        log('[page]sass编译');
         gulp.src(['dev/page/**/*.scss'])
             .pipe(sass())
-            .pipe(gulp.dest('built/static/pages/'));
-        //普通css
+            .pipe(gulp.dest('built/static/pages/'))
+            .on('finish', function(){pageEngine.next('[page]sass编译完成')});
+        log('[page]普通css');
         gulp.src(['dev/page/**/*.css'])
-            .pipe(gulp.dest('built/static/pages/'));
-        //commonjs用browserify打包
+            .pipe(gulp.dest('built/static/pages/'))
+            .on('finish', function(){pageEngine.next('[page]css复制完成')});
+        log('[page]commonjs用browserify打包');
         gulp.src(['dev/page/**/*.common.js'])
             .pipe(browserify())
             .pipe(rename(function(file){
                 file.basename = file.basename.replace(/.common$/, '');
             }))
-            .pipe(gulp.dest('built/static/pages/'));
-        //其他渣渣js
+            .pipe(gulp.dest('built/static/pages/'))
+            .on('finish', function(){pageEngine.next('[page]commonjs完成')});
+        log('[page]其他渣渣js');
         gulp.src(['dev/page/**/*.js', '!*.common.js'])
-            .pipe(gulp.dest('built/static/pages/'));
-        //其他渣渣资源
+            .pipe(gulp.dest('built/static/pages/'))
+            .on('finish', function(){pageEngine.next('[page]js复制完成')});
+        log('[page]其他渣渣资源');
         gulp.src(['dev/page/*/**', '!**/*.js', '!**/*.css', '!**/*.scss', '!**/*.html'])
-            .pipe(gulp.dest('built/static/pages/'));
+            .pipe(gulp.dest('built/static/pages/'))
+            .on('finish', function(){pageEngine.next('[page]静态资源复制完成')});
     });
     gulp.task('static', function(){
-        //sass编译
+        log('[static]sass编译');
         gulp.src(['dev/static/css/*.scss'])
             .pipe(sass())
             .pipe(gulp.dest('built/static/css/'));
-        //普通css
+        log('[static]普通css');
         gulp.src(['dev/static/css/*.css'])
             .pipe(gulp.dest('built/static/css/'));
-        //images下面所有
+        log('[static]images下面所有');
         gulp.src(['dev/static/images/*'])
             .pipe(gulp.dest('built/static/images/'));
-        //commonjs用browserify打包
+        log('[static]commonjs用browserify打包');
         gulp.src(['dev/static/js/*.common.js'])
             .pipe(browserify())
             .pipe(rename(function(file){
                 file.basename = file.basename.replace(/.common$/, '');
             }))
             .pipe(gulp.dest('built/static/js/'));
-        //其他渣渣js
+        log('[static]其他渣渣js');
         gulp.src(['dev/static/js/*.js', '!dev/static/js/*.common.js'])
             .pipe(gulp.dest('built/static/js/'));
     });
 
 
-    gulp.task('default', ['layout','post','rootConfig','pages','pageResources', 'static']);
+    gulp.task('default', ['layout','post','rootConfig','pageResources','static']);
     gulp.start('default');
 }
--- bin/gulp-buildPage.js
+++ bin/gulp-buildPage.js
@@ -2,10 +2,13 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var pp = require('preprocess');
 
+var uglifyJs = require('uglify-js');
+
 var fs = require('fs');
 
 module.exports = function (options) {
     var targetDir = 'built';
+    var min = !!options.min;
     return through.obj(function (file, enc, cb) {
         // 如果文件为空，不做任何操作，转入下一个操作，即下一个 .pipe()
         if (file.isNull()) {
@@ -27,15 +30,23 @@ module.exports = function (options) {
         var jsLink = /<style>@import url\("([\w\-\/\.]+)"\);<\/style>/.exec(content);
 
         content = content.replace(/<style>@import url\("([\w\-\/\.]+)"\);<\/style>/, function(match, g1){
+                console.log('matching '+targetDir + g1);
                 if(fs.existsSync(targetDir + g1)){
-                    return '<style>' + fs.readFileSync(targetDir + g1) + '</style>';
+                    console.log('======== '+targetDir + g1 +' exists');
+                    return min ? 
+                        '<style>' + fs.readFileSync(targetDir + g1).toString().replace(/\s*\n\r?\s*/g, '') + '</style>':
+                        '<style>' + fs.readFileSync(targetDir + g1) + '</style>';
                 }
-                return match;
-            }).replace(/<script src="([\w\-\/\.]+)"\)"><\/script>/, function(match, g1){
+                return '';
+            }).replace(/<script src="([\w\-\/\.]+)"><\/script>/, function(match, g1){
+                console.log('matching '+targetDir + g1);
                 if(fs.existsSync(targetDir + g1)){
-                    return '<script>' + fs.readFileSync(targetDir + g1) + '</script>';
+                    console.log('======== '+targetDir + g1 +' exists');
+                    return min ? 
+                        '<script>' + uglifyJs.minify(fs.readFileSync(targetDir + g1).toString(), {fromString: true}).code + '</script>':
+                        '<script>' + fs.readFileSync(targetDir + g1) + '</script>';
                 }
-                return match;
+                return '';
             });
 
 
