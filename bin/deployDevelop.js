fec9a10675ec5915a3713031a1ea24e8ccb72443
chenwenbin03 <chenwenbin03@meituan.com>
2015-08-13 19:01:41 +0800

    add

--- bin/deployDevelop.js
+++ bin/deployDevelop.js
@@ -1,91 +1,90 @@
-var gulp = require('gulp'),
-    rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    browserify = require('gulp-browserify'),
-    watch = require('gulp-watch');
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 
+var browserify = require('browserify');
+var transform = require('vinyl-transform');
+
+var srcDir = './dev/',
+    destDir = './temp/';
+
+var shrinkDir = function(file){
+    var filename;
+    // filename = /([^\/]+)\/?$/.exec(file.pathname)[1];
+    file.pathname.replace(/([^\/]+)(\/?)$/, function(match, p1, p2){
+        filename = p1;
+        return p2 ? p2 : '/';
+    });
+    file.basename = file.basename.replace(/index/, filename);
+});
 module.exports = function(env){
+    var browserified = transform(function (filename) {
+        return browserify(filename, {debug: true})
+            // .add('es5-shim')
+            .bundle();
+    });
+    browserified.on('error', function(e){
+        // why?
+        // delete e.stream;
+        console.error(e.message);
+        this.emit('end');
+    });
+
     gulp.task('layout', function(){
         //装饰器
-        return gulp.src(['dev/dec/*.html'])
-            .pipe(watch('dev/dec/*.html'))
-            .pipe(replace(/{% include ga\.html %}/, ''))
-            .pipe(gulp.dest('built/_layouts/'));
+        return gulp.src([srcDir + 'dec/*.html'])
+            .pipe(gulp.dest(destDir + '_layouts/'));
     });
     gulp.task('post', function(){
         //菠萝格
-        return gulp.src(['posts/**'])
-            .pipe(gulp.dest('built/_posts/'));
+        return gulp.src([srcDir + 'posts/**'])
+            .pipe(gulp.dest(destDir + '_posts/'));
     });
     gulp.task('rootConfig', function(){
         //根目录配置文件
         return gulp.src(['root/*'])
-            .pipe(gulp.dest('built/'));
+            .pipe(gulp.dest(dest));
+    });
+    gulp.task('static', function(){
+        //css
+        gulp.src([srcDir + 'static/css/*.scss'])
+            .pipe($.sass())
+            .pipe(gulp.dest(destDir));
+        gulp.src(srcDir + 'static/css/*.css')
+            .pipe(gulp.dest(destDir));
+        //images下面所有
+        gulp.src([srcDir + 'static/images/*'])
+            .pipe(gulp.dest(destDir));
+        //lib下面
+        gulp.src([srcDir + 'static/lib/*/index.js'])
+            .pipe(browserified)
+            .pipe($.rename(shrinkDir))
+            .pipe(gulp.dest(destDir));
+        gulp.src([srcDir + 'static/lib/*.js'])
+            .pipe(gulp.dest(destDir));
     });
 
     gulp.task('pages', function(){
         //一个页面对应一个文件夹
-        gulp.src(['dev/page/*/index.html'])
-            .pipe(rename(function(file){
+        gulp.src([srcDir + 'pages/*/index.html'])
+            .pipe($.rename(function(file){
                 file.basename = file.dirname;
                 file.dirname = '';
             }))
-            .pipe(watch('dev/page/*/index.html'))
-            .pipe(gulp.dest('built/'));
+            .pipe(gulp.dest(destDir));
         //sass编译
     });
     gulp.task('pageResources', function(){
-        gulp.src(['dev/page/**/*.scss'])
-            .pipe(watch('dev/page/**/*.scss'))
-            .pipe(sass())
-            .pipe(gulp.dest('built/static/pages/'));
-        //普通css
-        gulp.src(['dev/page/**/*.css'])
-            .pipe(watch('dev/page/**/*.css'))
-            .pipe(gulp.dest('built/static/pages/'));
         //commonjs用browserify打包
-        gulp.src(['dev/page/**/*.common.js'])
-            .pipe(browserify())
-            .pipe(watch('dev/page/**/*.common.js'))
-            .pipe(rename(function(file){
-                file.basename = file.basename.replace(/.common$/, '');
-            }))
-            .pipe(gulp.dest('built/static/pages/'));
-        //其他渣渣js
-        gulp.src(['dev/page/**/*.js', '!*.common.js'])
-            .pipe(watch('dev/page/**/*.js'))
-            .pipe(gulp.dest('built/static/pages/'));
+        gulp.src([srcDir + 'pages/**/index.js'])
+            .pipe(browserified)
+            .pipe($.rename(shrinkDir))
+            .pipe(gulp.dest(destDir + 'static'));
         //其他渣渣资源
-        gulp.src(['dev/page/*/**', '!**/*.js', '!**/*.css', '!**/*.scss'])
-            .pipe(gulp.dest('built/static/pages/'));
-    });
-    gulp.task('static', function(){
-        //sass编译
-        gulp.src(['dev/static/css/*.scss'])
-            .pipe(watch('dev/static/css/*.scss'))
-            .pipe(sass())
-            .pipe(gulp.dest('built/static/css/'));
-        //普通css
-        gulp.src(['dev/static/css/*.css'])
-            .pipe(watch('dev/static/css/*.css'))
-            .pipe(gulp.dest('built/static/css/'));
-        //images下面所有
-        gulp.src(['dev/static/images/*'])
-            .pipe(gulp.dest('built/static/images/'));
-        //commonjs用browserify打包
-        gulp.src(['dev/static/js/*.common.js'])
-            .pipe(browserify())
-            .pipe(rename(function(file){
-                file.basename = file.basename.replace(/.common$/, '');
-            }))
-            .pipe(gulp.dest('built/static/js/'));
-        //其他渣渣js
-        gulp.src(['dev/static/js/*.js', '!dev/static/js/*.common.js'])
-            .pipe(watch('dev/static/js/*.js'))
-            .pipe(gulp.dest('built/static/js/'));
+        // gulp.src([srcDir + 'pages/*/**', '!**/*.js', '!**/*.css', '!**/*.scss'])
+        //     .pipe(gulp.dest('built/static/pages/'));
     });
 
 
-    gulp.task('default', ['layout','post','rootConfig','pages','pageResources','static']);
+    gulp.task('default', ['layout','post','rootConfig','static','pages','pageResources']);
     gulp.start('default');
 }
