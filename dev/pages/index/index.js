0bfea18eebfb1e77d6a94ba4ee8125160d09bb86
chanteP <541149506@qq.com>
2015-08-30 21:00:08 +0800

    backup

--- dev/pages/index/index.js
+++ dev/pages/index/index.js
@@ -1,14 +1,21 @@
 import $ from '../core'
+import Drawer from '../../common/components/drawer'
+
 $.register('index', ($) => {
+    var drawer = new Drawer;
+    drawer.setContent('<iframe class="blog-drawer" name="blogPage"></iframe>');
     return {
         init(){
-            console.log('index init')
+            $.evt(this.node)
+                .on('click', '[data-act="open"]', function(e){
+                    drawer.show({
+                        href : this.getAttribute('href')
+                    });
+                });
         },
         show(){
-            console.log('index show')
         },
         hide(){
-            console.log('index hide')
         }
     };
 });
