1906f1e1c63ee731a77035ea4d0b2bc56c2c3f47
chenwenbin03 <chenwenbin03@meituan.com>
2015-09-01 16:57:34 +0800

    publish

--- static/lib/base.js
+++ static/lib/base.js
@@ -182,13 +182,16 @@ module.exports = function ($) {
 };
 
 },{}],4:[function(require,module,exports){
-"use strict";
+'use strict';
 
 module.exports = function ($) {
     $.domReady(function () {
         // 安卓300ms延迟避免初始化fastclick
         // if(/Android/i.test(navigator.userAgent)){return;}
-        window.FastClick && window.FastClick.attach(document.body);
+        if (window.FastClick) {
+            window.FastClick.attach(document.body);
+            document.body.classList.add('fastclick_outer');
+        }
     });
 };
 
