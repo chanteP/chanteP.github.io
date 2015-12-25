636310e1c1bd0588f1f6c2e35d5bfb184bed65e6
chenwenbin03 <chenwenbin03@meituan.com>
2015-12-25 19:47:45 +0800

    fix

--- build.js
+++ build.js
@@ -1,1 +1,28 @@
-require('@mtfe/alpha/bin/build')(require('./package.json').alphaConfig)
+var fs = require('fs');
+var glob = require('glob');
+require('@mtfe/alpha/bin/build')(require('./package.json').alphaConfig, function(){
+    console.log('build views');
+    try{
+        fs.mkdirSync('./temp/pages');
+    }
+    catch(e){
+    }
+    (glob.sync('./app/views/*.html', null) || []).map((p) => {
+        var content = fs.readFileSync(p);
+        var viewName = /([^\/]+)\.html$/.exec(p);
+        var contentMain = header('main') + content;
+        var contentPage = header('page') + content;
+        fs.writeFileSync('./temp/' + viewName[1] + '.html', contentMain);
+        fs.writeFileSync('./temp/pages/' + viewName[1] + '.html', contentPage);
+    });
+});
+
+
+function header(layout){
+    return [
+        '---',
+        'layout: ' + layout,
+        '---',
+        ''
+    ].join('\n');
+}
