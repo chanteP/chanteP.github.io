32b08cff862dc4f64947c59aa10cdd0243f83fcc
chanteP <541149506@qq.com>
2015-12-26 03:42:08 +0800

    deploy backup

--- app/pages/lab/index.js
+++ app/pages/lab/index.js
@@ -4,7 +4,7 @@ import css from './style.scss'
 $.register('lab', ($) => {
     return {
         init : function(){
-            $.lazyload($.find('.page_lab', this.node), $.find('.page_lab', this.node), 'data-lazy', function(el, src){
+            $.lazyload($.find('.page_lab', this.node), $.find('.page_lab', this.node), 'data-lazyload', function(el, src){
                 el.src = src;
             });
         }
