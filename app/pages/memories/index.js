3615f07dc7517e7d41fbd63ac0ed1486564712fe
chanteP <541149506@qq.com>
2015-12-27 01:49:50 +0800

    deploy backup

--- app/pages/memories/index.js
+++ app/pages/memories/index.js
@@ -6,8 +6,13 @@ $.register('memories', () => {
         init(){
             $.nav.check(true);
             $.nav.set('memories');
+            [].map.call($.findAll('[data-lazyload]', this.node), (el) => {
+                el.style.height = el.clientWidth * 0.67 + 'px';
+            });
             $.lazyload($.find('.gallery_list', this.node), $.find('.gallery_list', this.node), 'data-lazyload', function(el, src){
                 el.src = src;
+                el.style.height = '';
+                el.parentNode.classList.remove('loading');
             });
         },
         show(){
