af8460e7e522849cb147a2c7faf9db10d92c08b3
chanteP <541149506@qq.com>
2015-12-26 18:09:56 +0800

    deploy backup

--- app/pages/memories/index.js
+++ app/pages/memories/index.js
@@ -5,7 +5,7 @@ $.register('memories', () => {
     return {
         init(){
             $.nav.check(true);
-            $.nav.show();
+            $.nav.set('memories');
             $.lazyload($.find('.gallery_list', this.node), $.find('.gallery_list', this.node), 'data-lazyload', function(el, src){
                 el.src = src;
             });
