33bd96eb83cc675c1d32e72f01c3741137093f90
chanteP <541149506@qq.com>
2015-12-27 21:26:10 +0800

    deploy backup

--- app/pages/lab/index.js
+++ app/pages/lab/index.js
@@ -4,9 +4,11 @@ import css from './style.scss'
 $.register('lab', ($) => {
     return {
         init : function(){
-            $.lazyload($.find('.page_lab', this.node), $.find('.page_lab', this.node), 'data-lazyload', function(el, src){
-                el.src = src;
-            });
+            setTimeout(() => {
+                $.lazyload($.find('.page_lab', this.node), $.find('.page_lab', this.node), 'data-lazyload', function(el, src){
+                    el.src = src;
+                });
+            }, 500);
         }
     };
 });
