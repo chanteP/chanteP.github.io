570df92a684f72ed993c5cf875d33069628bc874
chantep <541149506@qq.com>
2023-12-04 16:51:38 +0800

    feat: next

--- app/lib/plugins/deviceCheck.js
+++
@@ -1,7 +0,0 @@
-/** track@alpha:{"version":"0.2.43","build":"2015-12-18 22:34:52","hash":""} */
-export default ($) => {
-    if(!('defineProperty' in Object)){
-        alert('系统版本太低，暂不支持');
-        window.close();
-    }
-}
