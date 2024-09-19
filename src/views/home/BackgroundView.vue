fdb65df1aff1c9cccecf6358e76d43049dbf5fa0
chantep <541149506@qq.com>
2024-09-19 12:09:41 +0800

    fix: fallback

--- src/views/home/BackgroundView.vue
+++ src/views/home/BackgroundView.vue
@@ -8,6 +8,7 @@ import { getNoiseImg } from '@/utils/gl/getNoise';
 import cloudFrag from './backgroundRender/sunset.frag?raw';
 
 const $canvas = ref<HTMLCanvasElement>();
+const needFallback = ref(false);
 
 async function init() {
     const { inject, injectTexture } = simpleInit($canvas.value!, {
@@ -22,11 +23,19 @@ async function init() {
     injectTexture('uNoise', 0, noiseImage, {});
 }
 
-onMounted(init);
+async function setContext(){
+    try{
+        await init();
+    }catch(e){
+        needFallback.value = true;
+    }
+}
+
+onMounted(setContext);
 </script>
 
 <template>
-    <div class="background fill">
+    <div class="background fill" :class="{fallback: needFallback}">
         <canvas ref="$canvas" class="canvas"></canvas>
         <div class="mask"></div>
     </div>
@@ -51,6 +60,18 @@ onMounted(init);
         width: 100%;
         height: 100%;
         z-index: -1;
+    }
+
+    &.fallback {
+        background: linear-gradient(0deg, rgb(255, 244, 208), rgb(84, 203, 177));
+        mix-blend-mode: exclusion;
+        .canvas {
+            background: 
+                repeating-radial-gradient(#000 0 .0001%, #fff 0 .0002%) 50% 0 / 2500px 2500px, 
+                repeating-conic-gradient(#000 0 .0001%, #fff 0 .0002%) 60% 60% / 2500px 2500px;
+            background-blend-mode: difference;
+            opacity: 0.06;
+        }
     }
 }
 </style>
