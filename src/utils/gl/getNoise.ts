3ec96108f8841912dec9b2365d104ab7ac725d40
chantep <541149506@qq.com>
2024-09-19 19:15:35 +0800

    fix: noise import

--- src/utils/gl/getNoise.ts
+++ src/utils/gl/getNoise.ts
@@ -1,16 +1,17 @@
 import { loadImage } from '../loader';
+import noise256 from './noise.base64';
 
 async function getNoise(size: 256 | 512 | 1024 = 256) {
     if (size === 512) {
-        return import('./noise512.png');
+        return (await import('./noise512.png')).default;
     }
     if (size === 1024) {
-        return import('./noise1024.png');
+        return (await import('./noise1024.png')).default;
     }
-    return import('./noise.png');
+    return noise256;
 }
 
 export async function getNoiseImg(size: 256 | 512 | 1024 = 256) {
-    const src = (await getNoise(size)).default;
+    const src = await getNoise(size);
     return loadImage(src);
 }
